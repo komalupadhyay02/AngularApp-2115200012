8f3228279ce9983284e1cc64df73475e55d57f1b
komalupadhyay02 <komal.upadhyay_cs.da21@gla.ac.in>
2025-03-22 11:02:36 +0530

    [komal]Adding UC1

---
+++ AngularApp/Helloworld/.angular/cache/19.2.4/Helloworld/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-W5K365OH.js";
+import "./chunk-TSMHHEG2.js";
+import "./chunk-YYC6OYLK.js";
+import "./chunk-BY44XJWI.js";
+import "./chunk-RMUIWRBI.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
